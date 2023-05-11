4714b5ddc4488b3c5ba2236843b26efb15033cd1
YoukoKobayashi <kobayouyou@gmail.com>
2023-05-11 23:23:10 +0900

    contactコンポーネントまで

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import { Inter } from '@next/font/google'
-import styles from "@/styles/Home.module.css";
-import React from "react";
+import Container from "@/components/container";
+// import styles from "@/styles/Home.module.css";
+// import React from "react";
 import Hero from "./hero";
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
-    </>
+    </Container>
   );
 }
